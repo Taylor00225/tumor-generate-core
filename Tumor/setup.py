039ffae2c521361c2b862b309757a7d238f4ca52
Taylor00225 <111715975+Taylor00225@users.noreply.github.com>
2024-05-10 02:49:39 +0800

    tumor_1.0

---
+++ Tumor/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='demo',
+    version='1.0',
+    packages=[''],
+    package_dir={'': 'Tumor'},
+    url='https://github.com/Taylor00225/tumor-generate-core.git',
+    license='',
+    author='Taylor00225',
+    author_email='478793660@qq.com',
+    description='tumor generate'
+)
